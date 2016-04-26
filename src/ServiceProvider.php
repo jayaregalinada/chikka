8939ac4d605b86ccdd40874bb880a6b9066808f1
Jay Are Galinada <jayaregalinada@gmail.com>
2016-04-27 04:19:00 +0800

    Add service provider.

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Jag\Chikka;
 
+use GuzzleHttp\Client;
 use Illuminate\Support\ServiceProvider as BaseServiceProvider;
+use InvalidArgumentException;
 
 class ServiceProvider extends BaseServiceProvider
 {
@@ -11,14 +13,26 @@ class ServiceProvider extends BaseServiceProvider
 
     public function boot()
     {
+        if (! $this->app['config']->has('services.chikka')) {
+            throw new InvalidArgumentException('Please add chikka to your services configuration');
+        }
     }
 
     public function register()
     {
+        $this->app->singleton('chikka', function ($app) {
+            return new Chikka($app, Client::class);
+        });
+        $this->app->singleton('chikka.client', function ($app) {
+            return $app['chikka']->getClient();
+        });
+        $this->app->singleton('chikka.sender', function ($app) {
+            return new Sender($app['chikka']);
+        });
     }
 
     public function provides()
     {
-        return [Chikka::class];
+        return ['chikka'];
     }
 }
